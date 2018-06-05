b4d3c06cab3fc76e95a6e46f9f80d6924bc276b9
yahiabaiba <baibba@gmail.com>
2018-06-05 10:29:32 +0000

    Add files via upload

---
+++ tesst.php
@@ -0,0 +1,106 @@
+<html>
+<head>
+<title>CONTAINER TRACKING</title>
+<link href="http://www.mauri10.com//IphoneIcone/apple-touch-icon.png" rel="apple-touch-icon" />
+<link href="http://www.mauri10.com//IphoneIcone/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+<link href="http://www.mauri10.com//IphoneIcone/apple-touch-icon-167x167.png" rel="apple-touch-icon" sizes="167x167" />
+<link href="http://www.mauri10.com//IphoneIcone/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
+<link href="http://www.mauri10.com//IphoneIcone/icon-hires.png" rel="icon" sizes="192x192" />
+<link href="http://www.mauri10.com//IphoneIcone/icon-normal.png" rel="icon" sizes="128x128" />
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+  
+<script src="http://code.jquery.com/jquery-1.10.0.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.10.0.min.js"></script>
+<script type="text/javascript">
+$(function(){
+    $('#Input').change(function(){
+        var input = $('#Input').val().toUpperCase(),
+            status = input.substring(0,3);
+        if(
+
+status === "BMO"||
+status === "AMF"||
+status === "COZ"||
+status === "LOT"||
+status === "MAE"||
+status === "MCA"||
+status === "MCA"||
+status === "MCH"||
+status === "MCH"||
+status === "MCI"||
+status === "MCR"||
+status === "MRK"||
+status === "MRS"||
+status === "MSA"||
+status === "MSF"||
+status === "MSK"||
+status === "MSW"||
+status === "MVI"||
+status === "MWC"||
+status === "MWM"||
+status === "OCL"||
+status === "POC"||
+status === "PON"||
+status === "SCM"||
+status === "UXX"||
+status === "TGH"||
+status === "TOR"||
+status === "TRI"||
+status === "UES"||
+status === "COZ"){
+            $('form').attr('action', 'https://my.maerskline.com/tracking/#tracking/'+input);
+        }else if(
+status === "AMC"||
+status === "ANN"||
+status === "APD"||
+status === "APH"||
+status === "API"||
+status === "APL"||
+status === "APR"||
+status === "APZ"||
+status === "TCN"||
+status === "CAI"||
+status === "CGH"||
+status === "CGM"||
+status === "CGT"||
+status === "CMA"||
+status === "CMN"||
+status === "CNC"||
+status === "DVR"||
+status === "ECM"||
+status === "GES"||
+status === "MMC"||
+status === "NEP"||
+status === "NOL"||
+status === "NOS"||
+status === "NUS"||
+status === "OPD"||
+status === "OTA"||
+status === "STM"){
+            $('form').attr('action', 'http://www.cma-cgm.com/ebusiness/tracking/search?SearchBy=Container&Reference='+input);
+        }
+    });
+});
+</script>
+</head>
+<body bgcolor="#000000">
+<div class="container" id="sc_feed">  
+	<h1>  &#1583;&#1610;&#1585; &#1585;&#1602;&#1605; &#1603;&#1608;&#1606;&#1578;&#1606;&#1610;&#1585;&#1578;&#1603; &#1607;&#1608;&#1606;</h1></div>
+
+<form action="" method="post" target="_blank">
+       <div class="input-group">
+        <input id="Input" name="Input" class="form-control" placeholder="ContainerNo" style="text-transform: uppercase"> 
+		 <div class="input-group-btn">
+           
+                <button id="Search" class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+      </div>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
